3622a8a4200ba6ca0be809416532096b7a2333d4
miiinhoo <154577329+miiinhoo@users.noreply.github.com>
2024-02-28 15:04:26 +0900

    Add files via upload

---
+++ js/fullpage-main.js
@@ -0,0 +1,80 @@
+$(function () {
+  let addClassOn = $("nav ul li");
+  const h2animation = $(".section h2 span");
+  let showText = $("#section2 svg");
+  let hideText = $("#section2 svg");
+  
+  $("#fullpage").fullpage({
+    menu: '#menu',
+    css3: true,
+    navigation: true,
+    navigationPosition: 'right',
+    autoScrolling: true,
+    scrollingSpeed:700,
+    lookAnchors:false,
+    anchors: ['page1', 'page2', 'page3', 'page4', 'last'],
+    responsiveWidth:1024,
+    'afterLoad': function (anchorLink, index) {
+      h2animation.css({ 'opacity': '0' })
+      
+      switch (index) {
+        
+        case 1:
+          addClassOn.eq(0).addClass("on").siblings().removeClass("on");
+          h2animation.animate({ opacity: '1' }, 3000);
+          break;
+        case 2:
+          addClassOn.eq(1).addClass("on").siblings().removeClass("on");
+          $(".inner-wrap.before").addClass("on");
+          break;
+        case 3:
+          addClassOn.eq(2).addClass("on").siblings().removeClass("on");
+          break;
+        case 4:
+          addClassOn.eq(3).addClass("on").siblings().removeClass("on");
+          break;
+        default:
+          break;
+      }
+    },
+    'onLeave': function (index, nextIndex, direction) {
+
+      switch (index) {
+        case 1:
+          if (direction === "down") { 
+            showText.show();
+          }
+          break;
+        case 2:
+          if (direction === "down") {    
+            $(".inner-wrap.before").removeClass("on");
+          }
+          if (direction === "up") {    
+            hideText.hide();
+            $(".inner-wrap.before").removeClass("on");
+          }
+          break;
+         case 3:
+          if (direction === "down") {    
+            $(".webPageInfo-textArea").removeClass('on');
+          }
+          if(direction === "up"){     
+            $(".webPageInfo-textArea").removeClass('on');
+          };
+          break;
+         case 4:
+          if (direction === "down") {   
+            
+          }
+          if (direction === "up") {    
+            
+          }
+          break;
+        default:
+   
+          break;
+      }
+    },
+  });
+
+});
---
+++ js/main.js
@@ -0,0 +1,144 @@
+$(function() {
+  //HEADER LOGO
+const content = 'MINO';
+const text = document.querySelector("header h1 a");
+let i = 0;
+
+function typing(){
+  let txt = content[i++];
+  text.innerHTML += txt;
+  if (i > content.length) {
+    text.textContent = "";
+    i = 0;
+  }
+}
+setInterval(typing, 400);
+
+  const openValue = document.querySelector('.clickOpen');
+  const openPage = document.querySelector('.responsiveClickEvent');
+  const closeValue = document.querySelector('.thisClose');
+  const $screenWidth768 = 768;
+  const $screenWidth1024 = 1024;
+
+  openValue.addEventListener('click',function(){
+    openPage.classList.add('on');
+    closeValue.style.display = 'block'
+  });
+  closeValue.addEventListener('click',function(){
+    openPage.classList.remove('on');
+    closeValue.style.display = 'none'
+  });
+  function testWidth(){
+    const windowWidth = window.innerWidth;
+    if(windowWidth >= $screenWidth768){
+      closeValue.style.display = 'none';
+    }
+      if(windowWidth >= $screenWidth1024){
+        openPage.classList.remove('on');
+      }
+  }
+  
+ 
+  window.addEventListener('DOMContentLoaded',function(){
+    testWidth();
+  });
+  window.addEventListener('resize',function(){
+    testWidth();
+  });
+  //링크
+    $(".imgover_button.VONIN").click(function () {
+      window.open('https://miiinhoo.github.io/vonin/', '_blank');
+    });
+    $(".imgover_button.MLB").click(function () {
+      window.open('http://minhohome.dothome.co.kr/', '_blank');
+    });
+    $(".imgover_button.HAPPY").click(function () { 
+      window.open('http://minho1.dothome.co.kr/', '_blank');
+    });
+    $(".imgover_button.SHERLOCK").click(function () {
+      window.open('https://miiinhoo.github.io/sherlock/', '_blank');
+    });
+
+  //스와이퍼
+  const mySlider = new Swiper(".swiper.mySlider", {
+    centeredSlides: true,
+    loop: true,
+    effect: 'fade',
+    navigation:{
+      nextEl:'.swiper1024-next',
+      prevEl:'.swiper1024-prev',
+    },
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction:true,
+    },
+    speed:700,
+    initialSlide: 0
+  });
+  
+  $('.swiper-slide').hover(function(){
+    mySlider.autoplay.stop();
+  }, function(){
+    mySlider.autoplay.start();
+  });
+
+  
+  $('.webPageInfo').each(function() {
+  $(this).on('click', function() {
+    let textArea = $(this).closest(".swiper-slide").find('.webPageInfo-textArea');
+    textArea.addClass('on');
+  });
+  });
+  
+  $(".webPageInfo-textArea .closed").each(function () {
+    $(this).on('click', function () {
+      let close = $(this).parent();
+      close.removeClass("on");
+    })
+  });
+  
+$('.mySlider .swiper-wrapper.clone').addClass('on');
+$('.mySlider .swiper-wrapper.project').removeClass('on');
+
+
+  // li 클릭 이벤트 핸들러
+  $('#tabMenu > li').click(function() {
+    let swiperIndex = $(this).index();
+    $(this).addClass('on').siblings().removeClass('on');
+    $('.swiper-wrapper').removeClass('on');
+    $('.swiper-wrapper').eq(swiperIndex).addClass('on');
+
+    const selectedMenu = $(this).index();
+    const newTitle = selectedMenu === 0 ? 'CLONE CODING' : 'PROJECT';
+    $('.swiper.mySlider > h2.title').html(newTitle);
+  });
+
+
+
+  //이메일복사
+  let isAnimating = false;
+$(".copyMail").click(function() {
+  if (!isAnimating) {
+    isAnimating = true;
+    let email = $(".copyMail").text();
+    navigator.clipboard.writeText(email);
+    let blockOut = $(".block-out");
+    blockOut.animate({
+      'opacity': '1'
+    }, 1000, function() {
+      setTimeout(function() {
+        $(".icon-checked > span").addClass("on");
+        setTimeout(function() {
+          $(".icon-checked > span").removeClass("on");
+          blockOut.animate({
+            'opacity': '0'
+          }, 1000, function() {
+            isAnimating = false;
+          });
+        }, 3000);
+      }, 400);
+    });
+  }
+});
+});
+
